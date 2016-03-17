f09f26892050d2df7160aaf67677ac837b8bae02
Thanassis Koustenis <activea@gmail.com>
2016-03-18 00:15:48 +0200

    ola ok gia 2i fora

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCmd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //int i = 0;
+            for (int j=0;j<10;j++){
+            Console.WriteLine(j);
+            }
+            Console.WriteLine("Hello Worldxyz12");
+           Console.Read(); 
+        }
+    }
+}
